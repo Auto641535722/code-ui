0d7cf5746a3673e5c6a1d4202526715f62f92d3d
liaoxudong <xudong.liao@faw-vw.com>
2021-08-25 15:05:01 +0800

    初始化

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,84 @@
+<template>
+  <el-table
+    :data="tableData"
+    border
+    style="width: 100%">
+    <el-table-column
+      type="index"
+      width="50">
+    </el-table-column>
+    <el-table-column
+      prop="name"
+      label="代码值"
+      width="180">
+    </el-table-column>
+    <el-table-column
+      prop="name"
+      label="代码名称"
+      width="180">
+    </el-table-column>
+    <el-table-column
+      prop="date"
+      label="创建日期"
+      width="180">
+    </el-table-column>
+    <el-table-column
+      prop="date"
+      label="修改日期"
+      width="180">
+    </el-table-column>
+    <el-table-column
+      label="操作"
+      width="100">
+      <template slot-scope="scope">
+        <el-button type="text" size="small">查看</el-button>
+        <el-button type="text" size="small">编辑</el-button>
+      </template>
+    </el-table-column>
+  </el-table>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      tableData: [{
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1517 弄'
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1519 弄'
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1516 弄'
+      }]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+/*h1, h2 {*/
+/*  font-weight: normal;*/
+/*}*/
+/*ul {*/
+/*  list-style-type: none;*/
+/*  padding: 0;*/
+/*}*/
+/*li {*/
+/*  display: inline-block;*/
+/*  margin: 0 10px;*/
+/*}*/
+/*a {*/
+/*  color: #42b983;*/
+/*}*/
+</style>
